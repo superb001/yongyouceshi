c9a1298a7eed0ccf3ac895feb144f06cac478ebb
superb001 <371603233@qq.com>
2021-01-24 21:38:26 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+/**
+ * @ClassName: Test
+ * @Description: TODO
+ * @Author: L7O dachaoliu1@163.com
+ * @Date: 2021/1/24 21:29
+ * @Version: V1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(0);
+    }
+}
